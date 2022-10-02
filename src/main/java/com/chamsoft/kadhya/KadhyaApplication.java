a422302fd489847ca53334ced9e1ec904f0b121e
CHAMSEDDINE <toujani.chams@gmail.com>
2022-10-02 14:46:31 +0200

    first commit

---
+++ src/main/java/com/chamsoft/kadhya/KadhyaApplication.java
@@ -0,0 +1,13 @@
+package com.chamsoft.kadhya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadhyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadhyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chamsoft/kadhya/KadhyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamsoft.kadhya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadhyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
